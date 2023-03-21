5bb6aeaacb88bcf0c0a75fc3d744197192ae19a8
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2023-03-21 18:43:05 -0500

    intalamos las dependecias de babel y hacemos unas configuraciones

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,16 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js']
+    },
+    module:{
+        rules: [
+            {
+            test: /\.m?js$/, // Test declara que extensión de archivos aplicara el loader
+            exclude: /node_modules/, // Exclude permite omitir archivos o carpetas especificas
+            use:{
+                loader:'babel-loader' // Use es un arreglo u objeto donde dices que loader aplicaras
+                }
+            }
+        ]
     }
 }
